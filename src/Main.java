399f772617653a4b6094b883eea870208f7a8fdd
Behçet <behcetmuhammed>
2024-07-26 22:37:55 +0300

    vc3

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package week5.teknikIcerik3.src;
+
+
+public class Main {
+    public static void main(String[] args) {
+        patikaStore patikaStore = new patikaStore();
+        patikaStore.start();
+    }
+}
---
+++ src/patikaStore.java
@@ -0,0 +1,185 @@
+package week5.teknikIcerik3.src;
+
+import java.util.*;
+
+public class patikaStore {
+    Scanner input = new Scanner(System.in);
+    public void start(){
+        while (true) {
+            System.out.println("PatikaStore Ürün Yönetim Paneli !");
+            System.out.println("1 - Notebook İşlemleri");
+            System.out.println("2 - Cep Telefonu İşlemleri");
+            System.out.println("3 - Marka Listele");
+            System.out.println("4 - ID'ye Göre Ürün Filtreleme");
+            System.out.println("5 - Markaya Göre Ürün Filtreleme");
+            System.out.println("0 - Çıkış Yap");
+            System.out.print("Tercihiniz: ");
+            int tercih = input.nextInt();
+
+            switch (tercih) {
+                case 1:
+                    notebookIslemleri();
+                    break;
+                case 2:
+                    cepTelefonuIslemleri();
+                    break;
+                case 3:
+                    markaListele();
+                    break;
+                case 4:
+                    System.out.println("Filtrelemek istediğiniz id yi giriniz : ");
+                    int IdFiltre = input.nextInt();
+                    urunFiltreleById(IdFiltre);
+                    break;
+                case 5:
+                    input.nextLine(); // Önceki girdiyi temizle
+                    System.out.println("Filtrelemek istediğiniz markayı giriniz : ");
+                    String markayaFiltre = input.nextLine().toUpperCase();
+                    urunFiltreleByMarka(markayaFiltre);
+                    break;
+                case 0:
+                    System.out.println("Çıkış yapılıyor...");
+                    input.close();
+                    return;
+                default:
+                    System.out.println("Geçersiz bir tercih yaptınız!");
+            }
+        }
+    }
+    public static Map<String, Set<Urun>> urunler = new HashMap<>();
+    public static Set<Marka> markalar = new TreeSet<>((m1, m2) -> m1.getName().compareTo(m2.getName()));
+    public static int urunIdCounter = 1;
+    static {
+        markalar.add(new Marka(1, "SAMSUNG"));
+        markalar.add(new Marka(2, "LENOVA"));
+        markalar.add(new Marka(3, "APPLE"));
+        markalar.add(new Marka(4, "HUAWEİ"));
+        markalar.add(new Marka(5, "CASPER"));
+        markalar.add(new Marka(6, "ASUS"));
+        markalar.add(new Marka(7, "HP"));
+        markalar.add(new Marka(8, "XİAOMİ"));
+        markalar.add(new Marka(9, "MONSTER"));
+
+        urunler.put("Cep Telefonları", new HashSet<>());
+        urunler.put("Notebook", new HashSet<>());
+    }
+    public  void notebookIslemleri() {
+
+        while (true) {
+            System.out.println("Notebook İşlemleri");
+            System.out.println("1 - Notebook Listele");
+            System.out.println("2 - Notebook Ekle");
+            System.out.println("3 - Notebook Sil");
+            System.out.println("4 - Geri");
+            System.out.print("Tercihiniz: ");
+            int tercih = input.nextInt();
+
+            switch (tercih) {
+                case 1:
+                    Notebook.notebookListele();
+                    break;
+                case 2:
+                    Notebook.notebookEkle();
+                    break;
+                case 3:
+                    Notebook.notebookSil();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Geçersiz bir tercih yaptınız!");
+            }
+        }
+    }
+
+    public void cepTelefonuIslemleri() {
+
+        while (true) {
+            System.out.println("Cep Telefonu İşlemleri");
+            System.out.println("1 - Cep Telefonu Listele");
+            System.out.println("2 - Cep Telefonu Ekle");
+            System.out.println("3 - Cep Telefonu Sil");
+            System.out.println("4 - Geri");
+            System.out.print("Tercihiniz: ");
+            int tercih = input.nextInt();
+
+            switch (tercih) {
+                case 1:
+                    CepTelefonu.cepTelefonuListele();
+                    break;
+                case 2:
+                    CepTelefonu.cepTelefonuEkle();
+                    break;
+                case 3:
+                    CepTelefonu.cepTelefonuSil();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Geçersiz bir tercih yaptınız!");
+            }
+        }
+    }
+
+    public void markaListele() {
+        System.out.println("Markalarımız");
+        System.out.println("--------------");
+        for (Marka marka : markalar) {
+            System.out.println("- " + marka.getName());
+        }
+        System.out.println();
+    }
+    public void urunFiltreleById(int id) {
+        System.out.println("ID'ye Göre Ürün Filtreleme");
+        System.out.println("----------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-3s| %-30s| %-10s| %-10s| %-10s| %-10s| %-12s|\n", "ID", "Ürün Adı", "Fiyat", "Marka", "Depolama", "Ekran", "RAM");
+        System.out.println("----------------------------------------------------------------------------------------------------");
+
+        boolean urunBulundu = false;
+
+        for (Set<Urun> urunSet : urunler.values()) {
+            for (Urun urun : urunSet) {
+                if (urun.getId() == id) {
+                    urunBulundu = true;
+                    System.out.printf("| %-3d| %-30s| %-10.2f| %-10s| %-10d| %-10.1f| %-12d|\n",
+                            urun.getId(), urun.getUrunAdi(), urun.getFiyat(), urun.getMarka().getName(),
+                            urun.getDepolama(), urun.getEkran(), urun.getRam());
+                    break; // Bulunan ürünün ID'si eşleştiği için döngüden çık
+                }
+            }
+        }
+
+        if (!urunBulundu) {
+            System.out.println("Aradığınız ID'ye ait ürün bulunamadı.");
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------------------");
+    }
+
+    public void urunFiltreleByMarka(String markaAdi) {
+        System.out.println("Markaya Göre Ürün Filtreleme");
+        System.out.println("----------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-3s| %-30s| %-10s| %-10s| %-10s| %-10s| %-12s|\n", "ID", "Ürün Adı", "Fiyat", "Marka", "Depolama", "Ekran", "RAM");
+        System.out.println("----------------------------------------------------------------------------------------------------");
+
+        boolean urunBulundu = false;
+
+        for (Set<Urun> urunSet : urunler.values()) {
+            for (Urun urun : urunSet) {
+                if (urun.getMarka().getName().equalsIgnoreCase(markaAdi)) {
+                    urunBulundu = true;
+                    System.out.printf("| %-3d| %-30s| %-10.2f| %-10s| %-10d| %-10.1f| %-12d|\n",
+                            urun.getId(), urun.getUrunAdi(), urun.getFiyat(), urun.getMarka().getName(),
+                            urun.getDepolama(), urun.getEkran(), urun.getRam());
+                }
+            }
+        }
+
+        if (!urunBulundu) {
+            System.out.println("Aradığınız markaya ait ürün bulunamadı.");
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------------------");
+    }
+
+}
